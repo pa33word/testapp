1b04bd4f665999a8f646de2de8121eff4a061549
notroot <notroot@ubuntu.(none)>
2013-01-06 11:41:06 -0800

    test aes v1

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python corktest.py
+web: python app.py
--- app.py
+++ app.py
@@ -1,6 +1,39 @@
 import os
 import psycopg2
 from bottle import route, run, template
+## NEED below for encryption
+from Crypto.Cipher import AES
+from base64 import b64encode, b64decode
+from datetime import datetime
+from re import sub
+
+
+class aestest:
+	'Optional class documentation string'
+	BLOCK_SIZE = 32
+	INTERRUPT = u'\u0001'
+	PAD = u'\u0000'
+	
+	def __init__(self):
+		xx = u'12345678abcdeggh'
+
+	def EncryptWithAES(self, encrypt_cipher, plaintext_data):
+		def AddPadding(data, interrupt, pad, block_size):
+			new_data = ''.join([data, interrupt])
+			new_data_len = len(new_data)
+			remaining_len = block_size - new_data_len
+			to_pad_len = remaining_len % block_size
+			pad_string = pad * to_pad_len
+			return ''.join([new_data, pad_string])
+		plaintext_padded = AddPadding(plaintext_data, aestest.INTERRUPT, aestest.PAD, aestest.BLOCK_SIZE)
+		encrypted = encrypt_cipher.encrypt(plaintext_padded)
+		return b64encode(encrypted)
+	def DecryptWithAES(self, decrypt_cipher, encrypted_data):
+		def StripPadding(data, interrupt, pad):
+			return data.rstrip(pad).rstrip(interrupt)
+		decoded_encrypted_data = b64decode(encrypted_data)
+		decrypted_data = decrypt_cipher.decrypt(decoded_encrypted_data)
+		return StripPadding(decrypted_data, aestest.INTERRUPT, aestest.PAD)
 
 @route("/")
 def hello_world():
@@ -19,4 +52,24 @@ def test_db():
 	output = template('make_table', rows=result)
 	return output
 
+@route("/en")
+def test_en():
+	our_data_to_encrypt = u'The Sample encrypt text901234567890abc'
+	SECRET_KEY = u'a1b2c3d4e5f6g7h8a1b2c3d4e5f6g7h8' # possibly needs unicode conversion from input string
+		
+	IV = u'12345678abcdeggh' # possibly needs unicode conversion from input string
+	cipher_for_encryption = AES.new(SECRET_KEY, AES.MODE_CBC, IV)
+	cipher_for_decryption = AES.new(SECRET_KEY, AES.MODE_CBC, IV)
+	test = aestest()
+	encrypted_data = test.EncryptWithAES(cipher_for_encryption, our_data_to_encrypt)
+	print ('Encrypted string:', encrypted_data)
+
+
+@route("/de")
+def test_de():
+	test2 = aestest()
+	decrypted_data = test2.DecryptWithAES(cipher_for_decryption, encrypted_data)
+	print ('Decrypted string:', decrypted_data)
+##
+#
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
