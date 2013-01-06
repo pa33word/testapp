8dd294d907442da0a71e5b028df0a30adecf1ddc
notroot <notroot@ubuntu.(none)>
2013-01-06 06:28:57 -0800

    db test

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import os
+import psycopg2
 from bottle import route, run
 
 @route("/")
 def hello_world():
         return "Hello World! TESTING push"
+@route("/test")
+def test_db():
+	con = None
+	con = psycopg2.connect(database='dd593v8qlv784t', user='wovsovccagcayo') 
+	cur = con.cursor()
+	cur.execute('SELECT * FROM usertestx;') 
+	rows = cur.fetchall()	
+        #return "Hello World! TESTING push"
+	output = template('make_table', rows=result)
+	return output
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
