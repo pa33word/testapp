e7da5c432c561ef60305dc48a1b64ea90cdb23f8
notroot <notroot@ubuntu.(none)>
2013-01-06 06:43:56 -0800

    testdb11

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 import os
 import psycopg2
-from bottle import route, run
+from bottle import route, run, template
 
 @route("/")
 def hello_world():
         return "Hello World! TESTING push"
+
 @route("/test")
 def test_db():
 	con = None
-	con = psycopg2.connect(database='dd593v8qlv784t', user='wovsovccagcayo') 
+	con = psycopg2.connect(dbname="dd593v8qlv784t", host="ec2-54-243-217-241.compute-1.amazonaws.com", port="5432", user="wovsovccagcayo", password="ar47Vr0fsg86Hi_IwNcqLMcFSU")
+	
+#con = psycopg2.connect(database='mytestedb', user='postgres') 
 	cur = con.cursor()
 	cur.execute('SELECT * FROM usertestx;') 
-	rows = cur.fetchall()	
+	result = cur.fetchall()	
         #return "Hello World! TESTING push"
 	output = template('make_table', rows=result)
 	return output
